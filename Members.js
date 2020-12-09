fe25ec7d4cc6bc575c7353115a0c9ddc7c6ad84d
bsrisompong <bsrisompong@gmail.com>
2020-12-09 13:53:56 +0700

    fix problem [8]

--- Members.js
+++ Members.js
@@ -1,16 +1,16 @@
 const members = [
-    {
-        id: 1,
-        name: 'John Doe',
-        email: 'john@gmail.com',
-        status: 'active'
-    }
-    {
-        id: 2,
-        name: 'Bob Williams',
-        email: 'bob@gmail.com',
-        status: 'inactive'
-    }
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    status: 'active',
+  },
+  {
+    id: 2,
+    name: 'Bob Williams',
+    email: 'bob@gmail.com',
+    status: 'inactive',
+  },
 ]
 
 module.exports = members
